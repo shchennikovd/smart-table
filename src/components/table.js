9c36b68805872de4e63717b84850ced5b6cc88c3
Dima Shchennikov <shchennikovdp@yandex.ru>
2026-05-22 15:33:34 +0300

    version 1.1

--- src/components/table.js
+++ src/components/table.js
@@ -12,12 +12,54 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(item => {
+        const element = cloneTemplate(item);
+        
+        root[item] = element;
+
+        root.container.prepend(element.container);
+    });
+
+    after.forEach(item => {
+        const element = cloneTemplate(item);
+
+        root[item] = element;
+
+        root.container.append(element.container);
+    });
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
 
+    root.container.addEventListener('change', (event) => {
+        onAction();
+    })
+
+    root.container.addEventListener('reser', (event) => {
+        setTimeout(() => {
+            onAction();
+        });
+    })
+
+    root.container.addEventListener('submit', (event) => {
+        event.preventDefault();
+        onAction(event.submitter);
+    })
+
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+
+            Object.keys(item).forEach(key => {
+                if(!(key in row.elements)) {
+                    return;
+                }
+    
+                row.elements[key].textContent = item[key];
+            })
+
+            return row.container;
+        })
         root.elements.rows.replaceChildren(...nextRows);
     }
 
